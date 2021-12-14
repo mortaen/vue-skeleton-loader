dc1584f690605dea7875a7f42a7c033169d9a2d0
mortaen <kolbe.mor@gmail.com>
2021-12-14 11:00:36 +0100

    initial commit

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="profile-card">
+    <div class="profile-image">
+      <img :src="userData.pic" />
+    </div>
+    <div class="profile-info">
+      <span> Written By </span>
+      <h3>{{ userData.name }}</h3>
+      <p>{{ userData.bio }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+const loadUserData = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "mortaen",
+        pic: "https://thumb10.shutterstock.com/image-photo/redirected-150nw-151265393.jpg",
+        bio: `I want to learn more about vue, so I'm trying out this skeleton loader.`,
+      });
+    }, 2000);
+  });
+};
+export default {
+  async setup() {
+    const userData = ref(await loadUserData());
+    return {
+      userData,
+    };
+  },
+};
+</script>
+
+<style>
+.profile-card {
+  width: 100%;
+  max-width: 700px;
+  min-height: 180px;
+  background: white;
+  margin: 0 auto;
+  padding: 30px;
+  box-sizing: border-box;
+  overflow: hidden;
+  border-radius: 20px;
+}
+.profile-image {
+  width: 10%;
+  float: left;
+}
+.profile-image img {
+  width: 100%;
+  height: auto;
+  border-radius: 50%;
+  border: 5px solid #42b883;
+  padding: 5px;
+}
+.profile-info {
+  width: 85%;
+  float: right;
+}
+.profile-info span {
+  text-transform: uppercase;
+  color: #777;
+  letter-spacing: 3px;
+}
+.profile-info h3 {
+  margin: 10px 0;
+  font-size: 1.5em;
+  color: #222;
+}
+.profile-info p {
+  line-height: 140%;
+  color: #777;
+}
+</style>
